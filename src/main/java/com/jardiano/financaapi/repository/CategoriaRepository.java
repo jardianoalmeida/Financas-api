406670aeb84c126f9b125446bf22792417e933e9
Jardiano Almeida <jardiano.almeida@gmail.com>
2020-03-02 12:58:12 -0300

    Consultando primeiro recurso com GET

---
+++ src/main/java/com/jardiano/financaapi/repository/CategoriaRepository.java
@@ -0,0 +1,9 @@
+package com.jardiano.financaapi.repository;
+
+import com.jardiano.financaapi.model.Categoria;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+
+public interface CategoriaRepository extends JpaRepository<Categoria, Long> {
+
+}
---
+++ src/main/java/com/jardiano/financaapi/resource/CategoriaResource.java
@@ -0,0 +1,25 @@
+package com.jardiano.financaapi.resource;
+
+import java.util.List;
+
+import com.jardiano.financaapi.model.Categoria;
+import com.jardiano.financaapi.repository.CategoriaRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+
+@RestController
+@RequestMapping("/categorias")
+public class CategoriaResource {
+
+	@Autowired
+	private CategoriaRepository categoriaRepository;
+	
+	@GetMapping
+	public List<Categoria> listar() {
+		return categoriaRepository.findAll();
+	}
+	
+}
